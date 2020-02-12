b29ad8586caef39ba2250de091f2e47b0a6f55b4
Deekshith <S538365@nwmissouri.edu>
2020-02-12 10:19:12 -0600

    button added

---
+++ index.js
@@ -0,0 +1,12 @@
+
+
+
+
+//get a reference to the element
+var myBtn = document.getElementById('footer');
+
+//add event listener
+myBtn.addEventListener('click', function(event) {
+  console.log("hello")
+  Window.alert("hello!");
+});
